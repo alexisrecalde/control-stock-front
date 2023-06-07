0a7313afda388dbef79efdeb852c7f161d00dc05
claudioescobar <escobarc697@gmail.com>
2023-06-07 20:15:04 -0300

    Agregar producto

--- src/app/cobrar/page.js
+++ src/app/cobrar/page.js
@@ -1,7 +1,30 @@
-import React from "react";
+"use client"
+import { useState } from 'react';
 
 const Cobrar = () => {
-  return <div>Cobrar</div>;
+  const [productoSeleccionado, setProductoSeleccionado] = useState(null);
+
+  const handleCobro = () => {
+    if (productoSeleccionado) {
+      // Aquí puedes realizar cualquier lógica adicional para el cobro del producto
+      console.log(`Producto cobrado. ID: ${productoSeleccionado}`);
+    } else {
+      console.log('No se ha seleccionado ningún producto');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Cobro de productos</h2>
+      <select value={productoSeleccionado} onChange={(e) => setProductoSeleccionado(e.target.value)}>
+        <option value="">Selecciona un producto</option>
+        <option value="1">Producto 1</option>
+        <option value="2">Producto 2</option>
+        <option value="3">Producto 3</option>
+      </select>
+      <button onClick={handleCobro}>Cobrar</button>
+    </div>
+  );
 };
 
 export default Cobrar;
--- src/app/components/ItemDetailTable/ItemStock/ItemStock.js
+++ src/app/components/ItemDetailTable/ItemStock/ItemStock.js
@@ -7,6 +7,9 @@ import ButtonEliminar from "../../button/ButtonEliminar";
 import { TableContainer } from "@mui/material";
 import { useMutationDeleteProduct } from "@/app/utils/products/hooks/mutation";
 import Swal from "sweetalert2";
+import Dialog from "@mui/material/Dialog";
+import { useForm } from "react-hook-form";
+import Button from "@mui/material/Button";
 
 export default function ItemStock({ data }) {
   const [selectRows, setSelectRows] = useState([]);
@@ -55,6 +58,12 @@ export default function ItemStock({ data }) {
     },
   ];
 
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const openAddProductDialog = () => {
+    setOpenDialog(true);
+  };
+
   const { mutate: mutateDeleteProduct } = useMutationDeleteProduct();
 
   const deleteProduct = () => {
@@ -95,6 +104,36 @@ export default function ItemStock({ data }) {
         }
       });
   };
+
+  const { register, handleSubmit, reset } = useForm({ defaultValues: {} });
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch('http://localhost:8080/api/products', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log("Datos enviados correctamente:", responseData);
+        // Realizar cualquier otra acción después de subir los datos a la base de datos
+      } else {
+        console.error("Error al enviar los datos:", response.status);
+        // Manejar el error de acuerdo a tus necesidades
+      }
+    } catch (error) {
+      console.error("Error de red:", error);
+      // Manejar el error de red de acuerdo a tus necesidades
+    }
+  
+    reset(); // Limpia los campos del formulario
+  };
+  
+
   return (
     <div style={{ paddingBottom: "100px" }}>
       <TableContainer style={{ maxHeight: 800 }}>
@@ -119,7 +158,51 @@ export default function ItemStock({ data }) {
         <div
           style={{ display: "flex", justifyContent: "end", margin: "20px 0" }}
         >
-          <ButtonEliminar ids={selectRows} onClickAction={deleteProduct} />
+          <ButtonEliminar
+            tittle="Agregar"
+            onClickAction={openAddProductDialog}
+          />
+          <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+            <form className="addProduct" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                {...register("id", { required: true })}
+                placeholder="ID"
+              />
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                placeholder="Nombre"
+              />
+              <input
+                type="text"
+                {...register("description", { required: true })}
+                placeholder="Descripción"
+              />
+              <input
+                type="number"
+                {...register("quantity", { required: true })}
+                placeholder="Cantidad"
+              />
+              <input
+                type="number"
+                {...register("price", { required: true })}
+                placeholder="Precio"
+              />
+              <Button type="submit" >
+                Agregar Producto
+              </Button>
+              <Button onClick={() => setOpenDialog(false)}>Cancelar</Button> 
+            </form>
+            
+          </Dialog>
+          <ButtonEliminar
+            disabled={selectRows.length > 0 ? false : true}
+            ids={selectRows}
+            onClickAction={deleteProduct}
+            tittle="Eliminar"
+            color="error"
+          />
         </div>
       </TableContainer>
     </div>
