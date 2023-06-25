35a4f04b9bc0b4a19230755ab7d0de81490c6759
claudioescobar <escobarc697@gmail.com>
2023-06-25 17:01:13 -0300

    addSale2

--- src/app/components/ItemVentas/ItemSales.jsx
+++ src/app/components/ItemVentas/ItemSales.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
 
-
-export const ItemSales = (data) => {
-    console.log(data);
+export const ItemSales = ({ data }) => {
+  console.log(data);
+  
 
   return (
-    <div>ItemSales</div>
-    
-    
-  )
-}
+    <>
+      <div>ItemSales</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Compra</th>
+            <th>Detalle</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((data) => (
+            <tr>
+              <td>{data.id}</td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
